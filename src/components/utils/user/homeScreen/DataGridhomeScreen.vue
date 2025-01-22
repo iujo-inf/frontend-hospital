1bf2faccb5acc3fb884242cb5217f244bd587b8f
gusi <gustavo.20.guerrero@gmail.com>
2025-01-21 20:49:15 -0400

    homeScreen Gustavo Guerrero

---
+++ src/components/utils/user/homeScreen/DataGridhomeScreen.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="login-container">
+    <h1>Inicio de Sesión</h1>
+    <form @submit.prevent="handleSubmit">
+      <label for="username">Usuario:</label>
+      <input type="text" id="username" v-model="username" required>
+      <label for="password">Contraseña:</label>
+      <input type="password" id="password" v-model="password" required>
+      <router-link class="nav-link d-flex align-items-center" to="/">
+     <span  class="btn btn-primary">Iniciar Sesión</span>
+      </router-link>
+    </form>
+    <p>¿No tienes cuenta? <router-link to="/register">Regístrate</router-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    handleSubmit() {
+      // Lógica para enviar los datos al servidor y manejar la respuesta
+      this.$axios.post('/api/login', {
+        username: this.username,
+        password: this.password
+      })
+      .then(response => {
+        // Manejar la respuesta exitosa (e.g., almacenar el token, redirigir)
+        console.log(response.data);
+      })
+      .catch(error => {
+        // Manejar los errores (e.g., mostrar un mensaje de error)
+        console.error(error);
+      });
+    }
+  }
+}
+</script>
+<style>
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+}
+header {
+    text-align: center;
+}
+form {
+    border: 1px solid #ccc;
+    padding: 20px;
+}
+</style>
---
+++ src/views/homeScreen/homeScreenView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="loguin">
+    <BaseLayout page-title="Inicio">
+      <DataGridhomeScreen /> <!-- Este es el componente hijo que se pasará al slot -->
+    </BaseLayout> <!-- Asegúrate de que este componente esté en el template -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import DataGridhomeScreen from '@/components/utils/user/homeScreen/DataGridhomeScreen.vue';
+
+@Options({
+  components: {
+    DataGridhomeScreen, // Asegúrate de registrar el componente aquí
+  },
+})
+export default class homeScreenView extends Vue {}
+</script>
+
+<style scoped>
+
+</style>
